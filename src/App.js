769bb76ccbd5933fdb84afa9bed00ff579ac9b47
Patrick Hund <pahund@team.mobile.de>
2016-11-04 17:07:35 +0100

    added base URL to router, fix for issue #1
    Change-Id: Ifc8abd210cf8ed9c004fc40297ab4922556b2239

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Topics from './Topics';
 import { BrowserRouter, Match, Link } from 'react-router';
 import { rootElement, getDataAttr } from './util' ;
 
-const baseUrl = getDataAttr(rootElement, 'base-url') || '';
+const baseUrl = getDataAttr(rootElement, 'base-url') || '/';
 
 class App extends Component {
 
@@ -21,14 +21,14 @@ class App extends Component {
                     </div>
                     <nav className="App-navigation">
                         <ul>
-                            <li><Link to={`${baseUrl}/`}>Home</Link></li>
-                            <li><Link to={`${baseUrl}/about`}>About</Link></li>
-                            <li><Link to={`${baseUrl}/topics`}>Topics</Link></li>
+                            <li><Link to={`${baseUrl}`}>Home</Link></li>
+                            <li><Link to={`${baseUrl}about`}>About</Link></li>
+                            <li><Link to={`${baseUrl}topics`}>Topics</Link></li>
                         </ul>
                     </nav>
-                    <Match exactly pattern="/" component={Home} />
-                    <Match pattern="/about" component={About} />
-                    <Match pattern="/topics" component={Topics} />
+                    <Match exactly pattern={`${baseUrl}`} component={Home} />
+                    <Match pattern={`${baseUrl}about`} component={About} />
+                    <Match pattern={`${baseUrl}topics`} component={Topics} />
                 </div>
             </BrowserRouter>
         );
